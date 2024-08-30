59e6e60d1fe65ad5ab6083beddf46fe55598128a
Abhishek <you@example.com>
2024-08-30 12:41:33 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/vite-deploy/",
   server:{
     port:3000,
-    proxy: {
+    proxy: { 
       "/api": {
         target: "http://127.0.0.1:3000", 
         changeOrigin:true,
