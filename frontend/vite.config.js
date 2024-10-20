20de130c9d27ccb6f3f9a79e69787befb955de2d
Its-Abhishek-2907 <131296005+Its-Abhishek-2907@users.noreply.github.com>
2024-10-20 17:05:17 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
-    proxy: { 
+  // base:"/vite-deploy/",
+  server: {
+    port: 5173,
+    proxy: {
+      "/socket.io": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        ws: true
+      },
       "/api": {
         target: "http://localhost:3000",
-      },
-    },
-  },
+        changeOrigin: true
+      }
+    }
+  }
 });
